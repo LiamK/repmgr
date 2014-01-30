0e2fdb7dc6079da154384f3f04e1ec6a46cf304c
Liam Kirsher <liamk@numenet.com>
2014-01-30 14:15:35 -0800

    Add chef-solo search stuff

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,3 +1,4 @@
+require resolve
 include_recipe 'repmgr'
 package 'rsync'
 
@@ -32,14 +33,40 @@ if(node[:repmgr][:replication][:role] == 'master')
     end
   end
 else
-  master_node = search(:role, 'name:master').first
-#  master_node = discovery_search(
-#    'replication_role:master',
-#    :raw_search => true,
-#    :environment_aware => node[:repmgr][:replication][:common_environment],
-#    :minimum_response_time_sec => false,
-#    :empty_ok => false
-#  )
+  if Chef::Config[:solo]
+    master_node = search(:node, 'name:master').first
+    if master_node.nil?
+      raise "Master node not found!"
+    end
+    unless (defined? master_node[:repmgr][:addressing][:self] and not
+     master_node[:repmgr][:addressing][:self].empty?)
+      raise "Master host address definition required."
+    end
+    master_addr = master_node[:repmgr][:addressing][:self]
+    # Make sure we have a valid ip address, or resolvable host name
+    begin
+        Resolv.getaddress master_addr
+    rescue
+        raise "Invalid master host address: #{master_addr}"
+    end
+
+    # if these values are not set, then use some sensible defaults
+    unless master_node[:repmgr][:replication][:keep_segments]
+      master_node[:repmgr][:replication][:keep_segments] = 
+        node[:repmgr][:replication][:keep_segments]
+    end
+    unless master_node[:postgresql][:config][:port]
+      master_node[:postgresql][:config][:port] = 5432
+    end
+  else
+    master_node = discovery_search(
+      'replication_role:master',
+      :raw_search => true,
+      :environment_aware => node[:repmgr][:replication][:common_environment],
+      :minimum_response_time_sec => false,
+      :empty_ok => false
+    )
+  end
 
   unless(File.exists?(File.join(node[:postgresql][:config][:data_directory], 'recovery.conf')))
     # build our command in a string because it's long
